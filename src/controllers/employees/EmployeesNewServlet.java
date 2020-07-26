93322f8c9c22c4c924e3ec49741bc9de11dd46d1
misa.kameyama <rsz42916@nifty.com>
2020-07-27 08:20:07 +0900

    Add Employee new

---
+++ src/controllers/employees/EmployeesNewServlet.java
@@ -0,0 +1,34 @@
+package controllers.employees;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+
+@WebServlet("/employees/new")
+public class EmployeesNewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public EmployeesNewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("_token", request.getSession().getId());  // セッションスコープからセッションIDを取得する それを リクエストスコープに保存する
+        request.setAttribute("employee", new Employee());  // 空のEmployeeインスタンスをリクエストスコープに保存する
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+        rd.forward(request, response);
+
+    }
+
+
+}
